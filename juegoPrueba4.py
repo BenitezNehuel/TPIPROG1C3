f78a2789d73002e57353dd39c3e2e85d1cb51f80
Lau <lautaroolivero44@gmail.com>
2024-11-15 16:29:30 -0300

    actualizo juego y cargo dfd al repositorio

--- juegoPrueba4.py
+++ juegoPrueba4.py
@@ -4,6 +4,8 @@ escritorio_corrido = False
 escape = False
 llave = False
 ultravioleta = False
+
+#variables que dibujan con ASCII
 def candadoCerrado():
     print("       _____________ ")
     print("      |             |")
@@ -53,8 +55,9 @@ def mostrarCuadroReal():
     print( "| ##################### |")
     print( "|         1626          |")
     print( "+-----------------------+")
-
 def mostrarCuadro():
+
+
     
     print( "+-----------------------+")
     print( "|   #################   |")
@@ -71,6 +74,8 @@ def mostrarCuadro():
     print( "| ##################### |")
     print( "|         ####          |")
     print( "+-----------------------+")
+
+#bucle while principal(solo al abrir la puerta saldremos del bucle)
 while not escape :
     print("\n1. Mirar a tu alrededor\n2. Caminar por la habitacion\n3. Intentar abrir la puerta")
     intro = int(input("-->"))
@@ -89,7 +94,6 @@ while not escape :
             caminar_habitacion = int(input("-->"))
 
             if caminar_habitacion == 1:
-                if luces_prendidas:
                     print("Al tacto se siente como cualquier escritorio normal de madera. tiene un cajon que no puedes abrir")
             elif caminar_habitacion == 2:
                 print("Decidís dejar el escritorio y seguir explorando.")
@@ -111,26 +115,27 @@ while not escape :
                     print("Decidis no tocar el interruptor.")
                 elif correr_escritorio == 3:
                     print("Acomodas el escritorio en su lugar.")
-
+                    escritorio_corrido = False
         else:
             print("Ahora que hay luz, podes ver distintos objetos en la habitacion.")
 
             print("\n1. Examinar armario\n2. Examinar cama\n3. Examinar cuadro\n4.Examinar escritorio")
-            explorarHabitacion = int(input("-->"))
+            explorar_habitacion = int(input("-->"))
 
-            if explorarHabitacion == 1:
+            if explorar_habitacion == 1:
                 print("Abris el armario y encontras una LUZ ULTRAVIOLETA.")
                 ultravioleta = True  
-            elif explorarHabitacion == 2:
+
+            elif explorar_habitacion == 2:
                     print("Miras hasta por debajo de la cama pero no hay nada.")
-            elif explorarHabitacion == 3:
+            elif explorar_habitacion == 3:
                 if ultravioleta:
                     print("Examinás el cuadro unos minutos, y utilizando la luz ultravioleta notas algo raro en la parte baja del cuadro, parece una clave.")
                     print(mostrarCuadroReal())
                 else:
                     print("Examinas el cuadro unos minutos pero no encuentras nada particular")
                     print(mostrarCuadro())
-            elif explorarHabitacion == 4:
+            elif explorar_habitacion == 4:
                 print("Ahora que ves el escritorio con claridad, notas que el cajon tiene un candado cidrado con 4 numeros.")
                 print(candadoCerrado())
                 print("\n1. tocar interruptor\n2. abrir cajon")
@@ -143,17 +148,19 @@ while not escape :
                         luces_prendidas = False
                         print("Se apago la luz")
                 elif menu_escritorio == 2:
-                    print("ingrese la clave de 4 digitos:")
+                    print("ingresa la clave de 4 digitos:")
                     clave = int(input())
                     if clave == 1626:
                         print("Lograste abrir el cajon!. Conseguiste UNA LLAVE")
                         llave = True
+                    else:
+                        print("Esa no es la clave.")
 
                 
 
     elif intro == 3:
         if llave:
-            print("¡Abriste la puerta! ¡Sos libre!")
+            print("¡¡¡Felicidades!!!\n¡Abriste la puerta! ¡Sos libre!")
             escape = True
         else:
             print("La puerta está cerrada. Necesitás encontrar una llave.")
