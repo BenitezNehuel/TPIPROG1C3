b6700f6d755acd55929edb702b913f9ab36dbdeb
BenitezNehuel <nehuelbenitez2961@gmail.com>
2024-11-19 20:41:18 -0300

    Correción del commit anterior. Menú creado correctamente

---
+++ main.py
@@ -0,0 +1,10 @@
+#Importar juegos:
+
+#Mostrar menú
+def menu():
+    print("Ingrese el número correspondiente al juego que quiera iniciar:")
+    print("1) Juego de escapismo")
+    print("2) LightBot")
+
+if __name__ == "main":
+    menu()
