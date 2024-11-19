6fe27f25bd8645329b35677f44b6fc1f3318ba10
BenitezNehuel <nehuelbenitez2961@gmail.com>
2024-11-19 08:48:14 -0300

    Correcciones en la generación de niveles nuevos

--- lightbot.py
+++ lightbot.py
@@ -59,7 +59,7 @@ def juegoMenu():
                 movimiento(movimientos,posicion_jugador)
             else:
                 os.system('cls')
-                print("La secuencia ingresada excede la cantidad de movimientos permitodos en este momento.")
+                print("La secuencia ingresada excede la cantidad de movimientos permitidos en este momento.")
                 mostrarNivel()
         else:
             salir = True
@@ -117,7 +117,7 @@ def crearNivel(posicion_jugador):
     return nivel
 
 def mostrarNivel():
-    print("| Nivel",datos["nivel"],"| Puntos restantes",cantidadPuntos,"|")
+    print("| Nivel",datos["nivel"],"| Luces restantes",cantidadPuntos,"|")
     for i in range(filas):
         for j in range(columnas):
             print(nivel[i][j],end="")
@@ -144,19 +144,19 @@ def movimiento(movimientos, posicion_jugador):
                 print(posicion_jugador)
             case 2: #Izquierda
                 posicion_jugador[1] = posicion_jugador[1]-1
-                verificarLuz()
+                luz_encontrada = verificarLuz()
                 nivel[posicion_jugador[0]][posicion_jugador[1]] = "🤖"
                 mostrarNivel()
                 print(posicion_jugador)
             case 3: #Arriba
                 posicion_jugador[0] = posicion_jugador[0]-1
-                verificarLuz()
+                luz_encontrada = verificarLuz()
                 nivel[posicion_jugador[0]][posicion_jugador[1]] = "🤖"
                 mostrarNivel()
                 print(posicion_jugador)
             case 4: #Abajo
                 posicion_jugador[0] = posicion_jugador[0]+1
-                verificarLuz()
+                luz_encontrada = verificarLuz()
                 nivel[posicion_jugador[0]][posicion_jugador[1]] = "🤖"
                 mostrarNivel()
                 print(posicion_jugador)
@@ -178,8 +178,8 @@ def movimiento(movimientos, posicion_jugador):
             print("Reiniciando en",i,"segundos.")
         os.system("cls")
         nivel = nivel_copia
-        posicion_jugador[0] = 0
-        posicion_jugador[1] = 0
+        posicion_jugador[0] = ultima_luz_encontrada[0]
+        posicion_jugador[1] = ultima_luz_encontrada[1]
         mostrarNivel()
             
 def terminarNivel():
@@ -199,6 +199,8 @@ def verificarLuz():
         print("Luz encontrada")
         datos["luces_conseguidas"]+=1
         cantidadPuntos-=1
+        global ultima_luz_encontrada
+        ultima_luz_encontrada=[posicion_jugador[0],posicion_jugador[1]]
         actualizarEstadisticas()
         return True
 
@@ -214,6 +216,9 @@ def crearPuntos(cantidadPuntos,posicion_jugador):
 import random
 import time
 import os
+
+global ultima_luz_encontrada
+ultima_luz_encontrada=[0,0]
 
 #Inicio del juego - menú
 global datos
